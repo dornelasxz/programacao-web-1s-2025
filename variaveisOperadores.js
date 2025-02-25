860c4842b2ee2a68e92ac88324bc22674a25d372
dornelasxz <73553825+dornelasxz@users.noreply.github.com>
2025-02-25 19:54:08 -0300

    Update variaveisOperadores.js

--- variaveisOperadores.js
+++ variaveisOperadores.js
@@ -1,1 +1,26 @@
+// Declaração de variáveis e operações
+let num1 = 10, num2 = 5;
 
+// Operação de adição
+let soma = num1 + num2;
+console.log(`Resultado da adição entre ${num1} e ${num2} é ${soma}`);
+
+// Operação de subtração
+let subtracao = num1 - num2;
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+
+// Operação de multiplicação
+let multiplicacao = num1 * num2;
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+
+// Operação de divisão
+let divisao = num1 / num2;
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
+
+// Operação de módulo (resto da divisão)
+let modulo = num1 % num2;
+console.log(`Resultado do módulo entre ${num1} e ${num2} é ${modulo}`);
+
+// Operação de exponenciação
+let potencia = num1 ** num2;
+console.log(`Resultado da exponenciação entre ${num1} e ${num2} é ${potencia}`);
